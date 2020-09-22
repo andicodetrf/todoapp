644be2924fd0781346c6d040f3d879e7de44dede
andicodetrf <andrea_lau92@hotmail.com>
2020-09-22 19:57:47 +0800

    Updates

---
+++ precache-manifest.95d44a78eabac29df0a88f120ca9ff05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adcb9410ccdd6fd464ed1b22f573746a",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "3b7756c69b4076edaff6",
+    "url": "/todoapp/static/css/main.c18eab83.chunk.css"
+  },
+  {
+    "revision": "884be467eb42e8b7fe19",
+    "url": "/todoapp/static/js/2.56382778.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.56382778.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b7756c69b4076edaff6",
+    "url": "/todoapp/static/js/main.4303a544.chunk.js"
+  },
+  {
+    "revision": "1fae731447e52269ba8b",
+    "url": "/todoapp/static/js/runtime-main.6c6c774f.js"
+  }
+]);
